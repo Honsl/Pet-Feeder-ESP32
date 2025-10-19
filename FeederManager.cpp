32945c984dd5aed9c55f1599b1f596a1b25658d2
Honsl <ryan.honsl@gmail.com>
2025-10-18 21:17:07 -0400

    Added default position
    If no schedules set motor to center position

--- FeederManager.cpp
+++ FeederManager.cpp
@@ -6,17 +6,23 @@ bool FeederManager::checkFoodLevel() {
 }
 
 bool FeederManager::feed(String type) {
+
+  return false;
+}
+
+//Set the motor to "Zero" postion
+bool FeederManager::setup() {
   SCSCL sc;
   Serial1.begin(1000000, SERIAL_8N1, S_RXD, S_TXD);
   sc.pSerial = &Serial1;
   delay(1000);
-  sc.WritePos(1, 0, 0, 1500);//Servo(ID1) moves at max speed=1500, moves to position=0.
-  delay(500);
-  return false;
-}
+  int Pos = sc.ReadPos(1);
+  Serial.print("Feeder POS: ");
+  Serial.println(Pos,DEC);
+  if (Pos != 0) {
+    sc.WritePos(1, 0, 0, 1500);  //Servo(ID1) moves at max speed=1500, moves to position=0.
+  }
 
 
-bool FeederManager::setFeedTimes(String type) {
-
-  return false;
+  return true;
 }
